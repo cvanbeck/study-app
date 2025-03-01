ff8dad5898222b1e4e5a359e174480b9019efbfb
lcann <liamcannon93@gmail.com>
2025-03-01 15:49:39 +0000

    Fixed new note generation

--- backend/nodejs/models/Note.js
+++ backend/nodejs/models/Note.js
@@ -1,27 +1,25 @@
 import axios from "axios";
 
 export default class Note {
-    constructor(token) {
-        this.token = token;
-        this.name = this.generateNote();
+    constructor(padName) {
+        this.token = "0046013d69a1e2ac718b1ac01d69bd041c1168a36b822dac6a08d41db3003b18";
+        this.name = this.generateNote(padName);
     }
 
-    async generateNote() {
+    async generateNote(padName) {
         try {
             const response = await axios.post(
                 "http://localhost:9001/api/1.3.0/createPad",
                 {
-                    padID: "testpad",
-                    text: "hu",
+                    padID: padName,
                 },
                 {
-                    headers: { "Authorization": token }
+                    headers: { "Authorization": this.token }
                 }
             );
-            console.log(response);
-            return response.data;
+            return padName;
         } catch (error) {
-            console.error();
+            console.error(error);
             return "";
         }
 
