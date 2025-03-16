9dc104dcdd1d27e26aa7ac5ca02305158ec08ccf
lcann <liamcannon93@gmail.com>
2025-03-16 03:15:59 +0000

    Refactored notes, implemented join sessions by url parameters and session redirects

--- backend/nodejs/services/SessionService.js
+++ backend/nodejs/services/SessionService.js
@@ -7,9 +7,9 @@ export default class SessionService {
     }
 
     // Store a code with a note
-    async storeCode(code, id) { 
+    async storeCode(code, page, id) { 
         try {
-            const result = await this.dbContext.query("INSERT INTO SessionCodes VALUES (?, ?)", [code, id]);
+            const result = await this.dbContext.query("INSERT INTO SessionCodes VALUES (?, ?, ?)", [code, page, id]);
             console.log("Sucessfully stored");
         } catch (error) {
             console.error('Error querying Notes table:', error);
@@ -20,8 +20,7 @@ export default class SessionService {
     // Retrieves a note from the DB based on session code linked to Note ID
     async getNote(code) {
         try {
-            const result = await this.dbContext.query("SELECT id, name, content FROM SessionCodes JOIN Notes on SessionCodes.NoteID = Notes.id WHERE code = ?", [code]);
-
+            const result = await this.dbContext.query("SELECT id, name, content, page FROM SessionCodes JOIN Notes on SessionCodes.NoteID = Notes.id WHERE code = ?", [code]);
             // Ensure we have a valid result
             if (result && result.length > 0) {
                 const row = result[0]; // Assuming 'id' is unique, we take the first result
@@ -30,8 +29,7 @@ export default class SessionService {
                 const formattedRow = Object.fromEntries(
                     Object.entries(row).map(([key, value]) => [key.toLowerCase(), value])
                 );
-
-                return new Note(formattedRow); // Return a Note object
+                return {note: new Note(formattedRow), page: formattedRow.page}; // Return a Note object
             } else {
                 console.log(`Note with id ${id} not found`);
                 return null; // Return null if the note is not found
