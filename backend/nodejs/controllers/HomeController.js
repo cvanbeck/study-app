56f6eeb0581b0bc66ed26d9497480cd62aaf15d4
Bappity <bappitybup@gmail.com>
2025-03-19 15:30:33 +0000

    clean up the homepage and move most of its code into an example page

--- backend/nodejs/controllers/HomeController.js
+++ backend/nodejs/controllers/HomeController.js
@@ -12,56 +12,7 @@ export default class HomeController extends BaseController {
 
     // REQUIRED FOR ALL CONTROLLERS
     async index(req, res) {
-        let firstRow = null;
-        try {
-            const result = await this.db.query('SELECT * FROM Employee LIMIT 1', []);
-
-            // Extract the first row from the result
-            firstRow = result.rows ? result.rows[0] : result[0];
-
-            // Option 2: If you prefer to return JSON directly, you could do:
-            // res.json(firstRow);
-        } catch (error) {
-            console.error('Error querying Employee table:', error);
-            // Send an error response if the query fails
-            return res.status(500).json({ error: 'Database query error' });
-        }
-
         // Now you can simply use the view name without the folder
-        return res.render("index.ejs", { ...this.appData, user: req.session.user, dbItem: firstRow });
+        return res.render("index.ejs", { ...this.appData, user: req.session.user });
     }
-
-    // OPTIONAL, EXAMPLE FUNCTION (routes to (url)/example)
-    async example(req, res) {
-        const item = new ExampleItem({
-            name: "Sample Item",
-            description: "This is a detailed description of the item"
-        });
-
-        // Pass data to the view
-        return res.render("example", { ...this.appData, item });
-    }
-
-    // OPTIONAL, EXAMPLE FUNCTION (routes to (url)/ajaxTest)
-    async ajaxTest(req, res) {
-        const item = new ExampleItem({
-            name: "Test",
-            description: "Test"
-        });
-        
-        // Prepare the content to send as a partial
-        return res.renderPartial("partials/ajaxTest", { ...this.appData, message: "This is the AJAX loaded content!", item });
-    }
-
-    // OPTIONAL, EXAMPLE FUNCTION (routes to (url)/ajaxModalTest)
-    async ajaxModalTest(req, res) {
-        const item = new ExampleItem({
-            name: "Modal Test",
-            description: "This is content loaded in a Bootbox modal!"
-        });
-
-        // Render the partial view
-        return res.renderPartial("partials/ajaxTest", { ...this.appData, message: "This content is loaded in a Bootbox modal!", item });
-    }
-
 }
