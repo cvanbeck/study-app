d6e38e7bcba9771afd7cf644e31302f11a9454b4
squalor0 <148451398+squalor0@users.noreply.github.com>
2025-03-24 13:39:53 +0000

    Update ChatService.js

--- backend/nodejs/services/ChatService.js
+++ backend/nodejs/services/ChatService.js
@@ -87,7 +87,7 @@ export default class ChatService {
         }
         try {
             const response = await axios.post(
-                "https://ai.api.parsonlabs.com/v1/chat/completions",
+                "http://localhost:11434/",
                 {
                     model: "deepseek-r1:1.5b",
                     messages: this.conversationHistory, // IMPORTANT! the entire history must be pushed here, not just one single message.
