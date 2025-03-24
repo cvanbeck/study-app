b7259c7911885aabf4cdbf7ea48b6f512b3a66d2
Bappity <12457072+bappitybup@users.noreply.github.com>
2025-03-24 22:21:44 +0000

    fix views location account controller

---
+++ backend/nodejs/controllers/AccountController.js
@@ -0,0 +1,17 @@
+import BaseController from "./base/BaseController.js";
+
+export default class AccountController extends BaseController {
+    // REQUIRED FOR ALL CONTROLLERS
+    constructor(appData) {
+        super(appData); // REQUIRED
+        
+        // Access the database context from the shared appData object
+        this.db = appData.db; // OPTIONAL
+    }
+
+    // REQUIRED FOR ALL CONTROLLERS
+    async index(req, res) {
+        // Now you can simply use the view name without the folder
+        return res.render("index.ejs", { ...this.appData, user: req.session.user });
+    }
+}
