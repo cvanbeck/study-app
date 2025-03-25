dbf30bf2fc41d013d6efd93ef51a59f907540516
MichallSk <124592659+MichallSk@users.noreply.github.com>
2025-03-25 14:24:15 +0000

    Updated package.json to add test script, created a test directory structure, created tests for NotesController

---
+++ backend/nodejs/__tests__/controllers/ChatController.test.js
@@ -0,0 +1,8 @@
+import { jest } from '@jest/globals';
+import ChatController from '../../controllers/ChatController.js';
+
+describe('ChatController', () => {
+    test('placeholder test', () => {
+        expect(true).toBe(true);
+    });
+});
